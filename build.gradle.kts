dfbaa81f0629f90c6bfd5dc96146c92c56adc575
TE-SergioCarrilho <sergio.carrilho@sony.com>
2026-01-14 16:30:55 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,37 @@
 plugins {
     kotlin("jvm") version "2.2.21"
+    alias(libs.plugins.kotlin.serialization)
+    application
 }
 
 group = "com.sergiocarrilho"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("com.sergiocarrilho.AgentKt")
+}
+
+repositories {
+    mavenCentral()
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation(libs.koog.agents)
+    implementation(libs.koog.tools)
+    implementation(libs.koog.executor.openai.client)
+    implementation(libs.koog.features.event.handler)
+    implementation(libs.okio)
     testImplementation(kotlin("test"))
-}
-
-kotlin {
-    jvmToolchain(17)
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
 }
